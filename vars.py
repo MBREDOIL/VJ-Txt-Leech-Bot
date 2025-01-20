c71a2c33d1945b966b221703dce11d31aa019a18
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-01-20 12:42:47 +0530

    vars.py

--- vars.py
+++ vars.py
@@ -8,4 +8,3 @@ API_ID = int(environ.get("API_ID", "22182189"))
 API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-proxy = environ.get("proxy" , "http://13.208.56.180:80")
